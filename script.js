ddb024c0b43bc7fbcaeaa90b35aded9aa48ef2ee
NouraKamal <76662207+NouraKamal@users.noreply.github.com>
2021-05-24 01:30:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,4 @@
-// part1&2
-// -----
-// var myHttp=new XMLHttpRequest();
-// var allPosts;
-// myHttp.open("GET","https://jsonplaceholder.typicode.com/posts");
-// myHttp.send();
 
-// myHttp.addEventListener("readystatechange",function(){
-//     if(myHttp.readyState==4&&myHttp.status==200){
-// allPosts=JSON.parse(myHttp.response);
-// displayPosts()
-//     }
-// })
-// function displayPosts(){
-//     var cartoona=``;
-//     for(var i=0;i<allPosts.length;i++){
-//         cartoona+=` <div class="col-md-3"> 
-//         <h3>${allPosts[i].title}</h3>
-//         <p>${allPosts[i].body}</p>
-//         </div>`
-//     }
-//     document.getElementById("rowData").innerHTML=cartoona;
-// }
-// -----------------------------------------------------------------
-// part3
 var myHttp=new XMLHttpRequest();
 var allMovies;
 myHttp.open("GET","https://api.themoviedb.org/3/trending/all/day?api_key=298a9d7e45243c18797ec4d6fe3de417");
@@ -48,16 +24,3 @@ function displayMovies(){
     }
     document.getElementById("rowData").innerHTML=cartoona;
 }
-// ----------------------------------------------------------------------------------------------------------------------
-// // part 6
-// let person={
-//     name:"hambozo",
-//     age:26,
-//     salary:6000,
-//     calcSalary:function(){
-//         let calcTax=()=> this.salary*0.1;
-    
-//         return this.salary-calcTax();
-//     }
-// }
-// console.log(person.calcSalary());
